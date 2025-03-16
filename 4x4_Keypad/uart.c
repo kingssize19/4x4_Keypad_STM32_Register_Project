2bd2489c82d9aa720ade2a6995ce947a3e59a90a
Hayati Kılıç <83482374+kingssize19@users.noreply.github.com>
2025-03-16 22:28:46 +0300

    Create uart.c

---
+++ 4x4_Keypad/uart.c
@@ -0,0 +1,31 @@
+#include "uart.h"
+
+
+void uart_init(void)
+{
+	//GPIOA Enable
+	RCC->AHB1ENR |= (1U << 0);
+
+	// USART2 Enable
+	RCC->APB1ENR |= (1U << 17);
+
+	// Alternate Function Mode
+	GPIOA->MODER |= (2U << 4);
+	// AF7 for Usart2 TX
+	GPIOA->AFR[0] |= (7U << 8);
+
+	// Transmitter Enable
+	USART2->CR1 |= (1U << 3);
+	// USART Enable
+	USART2->CR1 |= (1U << 13);
+
+	// Baudrate 115200
+	USART2->BRR = 0x8B;
+
+}
+
+void send_char(char c)
+{
+	while (!(USART2->SR & (1U << 7)));		// is TX empty?
+	USART2->DR = c; // write value
+}
